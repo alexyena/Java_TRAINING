daa783e599b7764c14bbf283ad661315c20dd9fd
Oleksii Iena <oiena@deloitte.com>
2018-10-27 11:01:59 +0300

    Added Maven hibernate configs

--- src/main/java/com/oiena/pms/LoginController.java
+++ src/main/java/com/oiena/pms/LoginController.java
@@ -1,11 +1,6 @@
 package com.oiena.pms;
 
-import java.text.DateFormat;
-import java.util.Date;
 import java.util.Locale;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,13 +11,6 @@ public class LoginController {
 	
 	@RequestMapping(value = "login", method = RequestMethod.GET)
 	public String login(Locale locale, Model model) {
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
 		
 		return "pages/login";
 	}
---
+++ src/main/java/models/User.java
@@ -0,0 +1,8 @@
+package models;
+
+import javax.persistence.Table;
+import javax.persistence.Column;
+
+public class User {
+
+}
